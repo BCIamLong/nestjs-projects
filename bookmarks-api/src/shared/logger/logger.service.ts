0ed5df21bd40e7c4cd55233dd015425354c8523c
longhoang <longkai21112002@gmail.com>
2024-07-21 11:22:56 +0700

    add(boomkmarks-api): the reason why the built-in logger maybe doesn't implement dependencies injection

--- bookmarks-api/src/shared/logger/logger.service.ts
+++ bookmarks-api/src/shared/logger/logger.service.ts
@@ -5,4 +5,7 @@ export class LoggerService extends ConsoleLogger {
   constructor() {
     super();
   }
+  // * by default we can't use the built-in logger of NestJS as dependencies injection, and one of the reason that because we usually test logger right and dependencies injection is maybe make the testing process better
+  // * and to use dependencies injection we need to create the custom logger like this
+  // * https://docs.nestjs.com/techniques/logger we can see on the doc
 }
